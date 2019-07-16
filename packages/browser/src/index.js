b6b6a76430095efc9ebd3447307f47f2dc09ad9d
Patrick Brandt <patrick@fullstory.com>
2019-07-16 16:16:12 -0400

    FullStory FS API implemented

--- packages/browser/src/index.js
+++ packages/browser/src/index.js
@@ -1,10 +1,67 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, friend! - ${name}`;
-  return returnStr;
+const fs = () => window[window['_fs_namespace']];
+
+const ensureSnippetLoaded = () => {
+  const snippetLoaded = !!fs();
+  if (!snippetLoaded) {
+    throw Error('FullStory is not loaded, please ensure the FullStory snippet is executed before calling FullStory API functions')
+  }
+  return true;
+}
+
+const hasFullStoryWithFunction = (...testNames) => {
+  const functionsCreated = () => testNames.reduce((acc, current) => { return acc && fs()[current] }, true);
+  return ensureSnippetLoaded() && functionsCreated();
 };
 
-const awesomeFunction = () => 'I am just an Awesome Function';
+const wrapFunction = name => (...params) => {
+  if (hasFullStoryWithFunction(name)) {
+    return fs()[name](...params);
+  }
+  console.warn(`FS.${name} not ready`);
+  return null;
+};
 
-export default defaultAwesomeFunction;
+const wrappedFS = ['event', 'log', 'getCurrentSessionURL', 'identify', 'setUserVars', 'consent', 'shutdown', 'restart'].reduce((acc, current) => {
+  acc[current] = wrapFunction(current)
+  return acc;
+}, {});
 
-export { awesomeFunction };
+const event = wrappedFS.event;
+const log = wrappedFS.log;
+const getCurrentSessionURL = wrappedFS.getCurrentSessionURL;
+const identify = wrappedFS.identify;
+const setUserVars = wrappedFS.setUserVars;
+const consent = wrappedFS.consent;
+const shutdown = wrappedFS.shutdown;
+const restart = wrappedFS.restart;
+
+const isReady = () => {
+  if (ensureSnippetLoaded() && window._fs_loaded) {
+    return Promise.resolve();
+  }
+
+  const cb = fs()._fs_ready;
+
+  fs()._fs_ready = () => {
+    try {
+      if (typeof cb === 'function') {
+        cb();
+      }
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+}
+
+export {
+  event,
+  log,
+  getCurrentSessionURL,
+  identify,
+  setUserVars,
+  consent,
+  shutdown,
+  restart,
+  isReady
+};
--- packages/browser/test/index.js
+++ packages/browser/test/index.js
@@ -1,14 +1,1 @@
-import { assert } from 'chai';
-import defaultAwesomeFunction, { awesomeFunction } from '../src';
-
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, friend! - Patrick';
-    assert(defaultAwesomeFunction('Patrick') === expectedVal, 'Default not awesome :(');
-  });
-
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function';
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
-  });
-});
+// TODO: write tests x_x
