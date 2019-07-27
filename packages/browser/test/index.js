9e2187930d2c3af5ec55e871c44127aa310c0c4a
Patrick Brandt <patrick@fullstory.com>
2019-07-27 10:49:14 -0400

    adding test #1

--- packages/browser/test/index.js
+++ packages/browser/test/index.js
@@ -1,1 +1,17 @@
-// TODO: write tests x_x
+import { assert } from 'chai';
+import * as FullStory from '../src';
+
+describe('core', () => {
+  it('should define snippet functions', () => {
+    const functions = ['event',
+      'log',
+      'getCurrentSessionURL',
+      'identify',
+      'setUserVars',
+      'consent',
+      'shutdown',
+      'restart'];
+
+    functions.forEach(i => assert(typeof FullStory[i] === 'function'));
+  });
+});
