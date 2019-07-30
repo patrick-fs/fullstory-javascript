eea55e4211b24c431ba8eb20a163cf50170e43fa
Patrick Brandt <patrick@fullstory.com>
2019-07-30 08:02:07 -0400

    semvar update

--- packages/browser/test/index.js
+++ packages/browser/test/index.js
@@ -1,5 +1,7 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import * as FullStory from '../src';
+
+const testOrg = '123';
 
 describe('core', () => {
   it('should define snippet functions', () => {
@@ -14,11 +16,17 @@ describe('core', () => {
 
     functions.forEach(i => assert(typeof FullStory[i] === 'function', `${i} has not been exported from the FullStory module`));
   });
+
+  it('should throw error if called before init', () => {
+    expect(() => { FullStory.log(); }).to.throw();
+    FullStory.init(testOrg);
+    expect(() => { FullStory.log(); }).to.not.throw();
+  });
 });
 
 describe('init', () => {
   it('should add _fs_org value to window object', () => {
-    FullStory.init('123');
-    assert(window._fs_org === '123');
+    FullStory.init(testOrg);
+    expect(window._fs_org).to.equal(testOrg);
   });
 });
