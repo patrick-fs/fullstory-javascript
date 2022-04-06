dec332e80161752433ef04e80d7d36c582f28846
Patrick Brandt <patrick@fullstory.com>
2022-04-06 16:21:51 -0400

    fixing up stale rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 import copy from 'rollup-plugin-copy';
 
-export default [	
+export default [
 	{
 		input: 'src/index.js',
 		output: [
-			{ file: pkg.main, format: 'esm' }
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'esm' }
 		],
 		plugins: [
 			babel({
-				exclude: ['node_modules/**']
+				exclude: ['node_modules/**'],
+        babelHelpers: 'bundled'
 			}),
 			copy({
 				targets: [
